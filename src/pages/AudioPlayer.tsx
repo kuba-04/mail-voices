4dfe73b611332257fbabf1d758483fe88e997b60
jakub gladysz <jakub.gladysz@autenti.com>
2023-04-25 21:56:57 +0200

    spacing on bar

--- src/pages/AudioPlayer.tsx
+++ src/pages/AudioPlayer.tsx
@@ -1,6 +1,6 @@
 import { IonButton, IonIcon } from '@ionic/react';
 import { play, pause, playSkipBackCircle, playSkipForwardCircle } from 'ionicons/icons';
-import {useRef, useState} from "react";
+import { useRef, useState } from "react";
 import './AudioPlayer.css';
 
 const AudioPlayer = ({ message, source }) => {
@@ -48,46 +48,46 @@ const AudioPlayer = ({ message, source }) => {
         audio.currentTime = newTime;
     };
 
-    const handleLoadedMetadata = () => {
-        const audio = audioRef.current;
-
-        if (!audio) {
-            return;
-        }
-
-        setDuration(audio.duration);
+    const formatTime = (time: number) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
     };
 
     return (
         <div className="audio-player-container">
             <div className="ion-padding">
-                <h1>{message.subject}</h1>
+                <h1 className="audio-title">{message.subject}</h1>
                 <audio
                     ref={audioRef}
                     src={source}
                     onTimeUpdate={handleTimeUpdate}
-                    onLoadedMetadata={handleLoadedMetadata}
+                    onLoadedMetadata={() => setDuration(audioRef.current.duration)}
                 />
 
-                <div className="buttons-container">
-                    <IonButton onClick={() => handleSeek(-15)}>
-                        <IonIcon slot="icon-only" icon={playSkipBackCircle} />
+                <div className="audio-controls">
+                    <IonButton onClick={() => handleSeek(-15)} fill="clear" className="audio-control">
+                        <IonIcon slot="icon-only" icon={playSkipBackCircle} className="audio-icon" />
                     </IonButton>
-                    <IonButton onClick={togglePlay}>
-                        <IonIcon slot="icon-only" icon={isPlaying ? pause : play} />
+                    <IonButton onClick={togglePlay} fill="clear" className="audio-control">
+                        <IonIcon slot="icon-only" icon={isPlaying ? pause : play} className="audio-icon" />
                     </IonButton>
-                    <IonButton onClick={() => handleSeek(15)}>
-                        <IonIcon slot="icon-only" icon={playSkipForwardCircle} />
+                    <IonButton onClick={() => handleSeek(15)} fill="clear" className="audio-control">
+                        <IonIcon slot="icon-only" icon={playSkipForwardCircle} className="audio-icon" />
                     </IonButton>
                 </div>
 
-                <div className="ion-padding">
-                    <div className="progress-bar">
-                        <div className="progress-bar-fill" style={{ width: `${(currentTime / audioRef.current?.duration) * 100}%` }} />
-                    </div>
+                {/*<div className="audio-timeline">*/}
+                <div className="progress-bar">
+                    {/*<div className="audio-timeline-current" style={{ width: `${(currentTime / duration) * 100}%` }} />*/}
+                    <div className="progress-bar-fill" style={{ width: `${(currentTime / duration) * 100}%` }} />
+                </div>
+
+                <div className="audio-timeline-labels">
+                    <span>{formatTime(currentTime)}</span>
+                    <span>{formatTime(duration)}</span>
                 </div>
             </div>
-
         </div>
     );
 };
